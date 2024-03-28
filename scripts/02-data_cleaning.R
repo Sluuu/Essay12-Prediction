70bedb320b9d114c56f766e278b6b550a404cd1b
Sean Liu <yuhsiang.liu@mail.utoronto.ca>
2024-03-27 20:20:14 -0400

    Update data cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw data, only having Quaterbacks data that are before week 9 of season 2023.
+# Author: Sean Liu
+# Date: 27 March 2024
+# Contact: yuhsiang.liu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: install the "nflverse" package 
+
 
 #### Workspace setup ####
 library(tidyverse)
@@ -15,7 +15,7 @@ library(nflverse)
 
 cleaned_data <-
   load_player_stats(seasons = 2023) |> 
-  filter(season_type == "REG" & position == "QB" & week >= 10)
+  filter(season_type == "REG" & position == "QB" & week <= 9)
 
 #### Save data ####
 write_csv(cleaned_data, "data/analysis_data/cleaned_data.csv")
